6046d14ba2c2abd4363f6cf50859a83472aebf2e
Yichao Yu <yyc1992@gmail.com>
2012-06-11 00:22:54 +0800

    po, name, etc

--- src/config.c
+++ src/config.c
@@ -1,6 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2012~2012 by CSSlayer                                   *
- *   wengxt@gmail.com                                                      *
+ *   Copyright (C) 2012~2012 by Yichao Yu                                  *
+ *   yyc1992@gmail.com                                                     *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
