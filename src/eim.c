3ac1c91b42a26f0198906ae5147fdf44d9b2be9f
Yichao Yu <yyc1992@gmail.com>
2012-06-20 14:39:42 +0800

    go to single

--- src/eim.c
+++ src/eim.c
@@ -195,14 +195,36 @@ static boolean KeyThemeSelectFirst(FcitxKeyTheme *theme,
         return false;
     if (FcitxCandidateWordGetListSize(word_list) <= 0)
         return false;
-    /* First word? Maybe First word on current page is better.. */
+    /* .... this IS the first word on the page ....
+     * So what if I want to really get the first word then?
+     * go to first page and select?.....
+     */
     *retval = FcitxCandidateWordChooseByIndex(word_list, 0);
     return true;
+}
+
+static int KeyThemeFindSingle(FcitxCandidateWordList *word_list)
+{
+    FcitxCandidateWord *word;
+    int i = 0;
+    for (word = FcitxCandidateWordGetFirst(word_list);
+         word;
+         word = FcitxCandidateWordGetNext(word_list, word), i++) {
+             size_t len = word->strWord ? fcitx_utf8_strlen(word->strWord) : 0;
+             if (len == 1)
+                 return i;
+         }
+    return -1;
 }
 
 static boolean KeyThemeGotoSingle(FcitxKeyTheme *theme,
                                   INPUT_RETURN_VALUE *retval)
 {
+    int i;
+    int single_index;
+    int page_size;
+    int page_index;
+    int cur_page;
     FcitxInputState *input_state;
     FcitxCandidateWordList *word_list;
     input_state = FcitxInstanceGetInputState(theme->owner);
@@ -213,7 +235,27 @@ static boolean KeyThemeGotoSingle(FcitxKeyTheme *theme,
         return false;
     if (FcitxCandidateWordGetListSize(word_list) <= 0)
         return false;
-    return false;
+    single_index = KeyThemeFindSingle(word_list);
+    if (single_index < 0)
+        return false;
+    page_size = FcitxCandidateWordGetPageSize(word_list);
+    if (page_size <= 0)
+        return false;
+    page_index = single_index / page_size;
+    cur_page = FcitxCandidateWordGetCurrentPage(word_list);
+    if (cur_page < 0)
+        return false;
+    if (cur_page > page_index) {
+        for (i = cur_page - page_index;i > 0;i--) {
+            FcitxCandidateWordGoPrevPage(word_list);
+        }
+    } else {
+        for (i = page_index - cur_page;i > 0;i--) {
+            FcitxCandidateWordGoNextPage(word_list);
+        }
+    }
+    *retval = IRV_FLAG_UPDATE_INPUT_WINDOW;
+    return true;
 }
 
 static boolean FcitxKeyThemePreHook(void *arg, FcitxKeySym sym,
